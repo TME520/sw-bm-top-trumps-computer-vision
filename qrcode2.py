f1fd6f160f7f375c604397d2a8a3044f40739355
TME520 <tme520@gmail.com>
2024-01-05 21:27:22 +1100

    Identify current card via QR code (1)

---
+++ qrcode2.py
@@ -0,0 +1,26 @@
+import cv2
+import webbrowser
+
+cap = cv2.VideoCapture(0) 
+# initialize the cv2 QRCode detector 
+detector = cv2.QRCodeDetector()
+
+while True:
+	_, img = cap.read()
+	# detect and decode 
+	data, bbox, _ = detector.detectAndDecode(img) 
+	# check if there is a QRCode in the image 
+	if data: 
+		a=data 
+		print(f'a: {a}')
+		if a == 'SWTOPTRUMPSBM001':
+			print('SWTOPTRUMPSBM001 - First Order Stormtrooper (15/2/13/5/5)')
+		break
+	cv2.imshow("QRCODEscanner", img)
+	if cv2.waitKey(1) == ord("q"):
+		break
+
+b=webbrowser.open(str(a))
+cap.release()
+cv2.destroyAllWindows()
+
