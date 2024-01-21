5d7b7cffc2814b7c94745d907890327b1a7bf4b5
TME520 <tme520@gmail.com>
2024-01-21 17:27:11 +1100

    Implementing INIT stage (2)

---
+++ phase1/code_sample02.py
@@ -0,0 +1,6 @@
+cardData = {
+	'SWTOPTRUMPSBM001':{'cardName':'First Order Stormtrooper', 'style':15, 'ambition':2, 'pride':13, 'leadership':5, 'ttgl':5},
+	'SWTOPTRUMPSBM002':{'cardName':'Jango Fett', 'style':23, 'ambition':15, 'pride':35, 'leadership':35, 'ttgl':11}
+}
+
+print(cardData['SWTOPTRUMPSBM002']['ttgl'])
